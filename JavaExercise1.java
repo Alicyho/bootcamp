a4dabdda007735b889edb7ff47ffc7abd4b2017b
Alicyho <yanaliceho@gmail.com>
2026-04-07 21:50:10 +0800

    Update JavaExercise1.java

--- JavaExercise1.java
+++ JavaExercise1.java
@@ -1,77 +1,81 @@
+/**
+ * Expected Output:
+ * 
+ * I am going to add two numbers together:
+ * The first one: 31
+ * The second one: 42
+ * The sum is 73
+ * The subtraction reuslt is 11
+ * The another sum result is 130
+ * The variable varB is false
+ * The variable varC is true 
+ * The variable fnumber is -130.2
+ * The variable dnumber is -20.5
+ * The variable remainder is 1
+ * The variable result is 3
+ */
+// Primitive types and operators
 public class JavaExercise1 {
+
   public static void main(String[] args) {
-    // + - * /
 
-    // % (Find Remainder)
-    int remainder = 5 % 2;
-    System.out.println(remainder); // 1
-    System.out.println(5 % 6); // 5
+    System.out.println("I am going to add two numbers together:");
+    int first = 31;
+    int second = 42;
+    int total = first + second;
 
-    // ++ --
-    // += -= *= /=
-    int a = 10;
-    a = a + 1; 
-    a += 1;
-    a++;
-    ++a;
-    System.out.println(a); // 14
+    System.out.println("The first one: " + first);
+    System.out.println("The second one: " + second);
 
-    int b = 20;
-    b = b - 1;
-    b -= 1;
-    b--;
-    --b;
-    System.out.println(16);
+    // Add two numbers and print it out
+    // code here ...
+    System.out.println(("total"+total));
 
-    // + 2
-    int c = 10;
-    c = c + 2;
-    c += 2;
+    // subtraction
+    // code here with tips, pls fix ...
+    // subtract = first - second
 
-    int salary = 25000;
-    salary *= 3;
-    System.out.println(salary); // 75000
+    int a1 = 10;
+    int a2 = 1;
+    int total2 = a1 - a2;
+    System.out.println("The subtraction reuslt is " + total2);
 
-    // Naming Convension: Caml Case
-    double hourRate = 40.5;
-    int hoursPerDay = 7;
-    double todaySalary = hourRate * hoursPerDay;
-    System.out.println(todaySalary);
+    // another sum
+    byte b1 = 60;
+    byte b2 = 70;
+    int sum =b1+b2;
+    // sum = b1 + b2;
+    System.out.println("The another sum reuslt is " + sum);
 
-    // pre / post
-    int g = 10;
-    g++;
-    ++g;
-    
-    System.out.println(g); // 12
+    // declare two boolean variable with true and false value
+    // code here
+    boolean varB = false;
+    boolean varC = true;
+    System.out.println("The variable varB is " + varB);
+    System.out.println("The variable varC is " + varC);
 
-    // post ++
-    int k = g++ * 2;
-    System.out.println(k); // 24 (x 2 first, then assign, then ++)
-    System.out.println(g); // 13
+    // Reading Example
+    float fnumber = -130.20f; // By default, numbers are treated as double in Java.
+    // Use suffix 'f' with value to make it float.
+    double dnumber = -20.50;
+    System.out.println("The variable fnumber is " + fnumber);
+    System.out.println("The variable dnumber is " + dnumber);
 
-    // pre ++
-    int p = 10;
-    int k2 = ++p * 2;
-    System.out.println(k2); // 22
-    System.out.println(p); // 11
+    // Find reminder. Declare variable a & reminder
+    int a = 7;
+    int b = 3;
+    // Fix it ...
+    int remainder = a % b;
+    System.out.println("The variable remainder is " + remainder);
 
-    // Another presentation for Post++
-    int h = 10;
-    // int y = h++ * 2;
-    int y = h * 2;
-    h++;
-    System.out.println(y); // 20
-    System.out.println(h); // 11
-
-    // Another presentation for pre++
-    int j = 10;
-    // int e = ++j * 2;
-    ++j;
-    int e = j * 2;
-    System.out.println(e); // 22
-    System.out.println(j); // 11
-
-
+    // declare a variable x and assign value to it.
+    int e = 7;
+    int o = 8;
+    int y = 4;
+    int x = 3;
+    // code here ... define the x and assign value to it
+    int result = e - o * x / y;
+    System.out.println("The result is " + result);
   }
 }
+
